83bd7e48a8dff3c0d13cf9c8c017980747879d16
Geovana Aparecida de Lima <lima.geovana@alunoescolassociais.org.br>
2024-09-20 08:55:52 -0300

    length é uma propriedade que verifica a quantidade caracteres.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+'use strict'; //Modo restrito 
+//Este modo faz com que o JavaScript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação
+/* Consumo de API - https://viacep.com.br/ */
+
+// Função para limpar formulário
+const limparFormulario = () =>{
+    document.getElementById('logradouro').value = '';
+    document.getElementById('uf').value = '';
+    document.getElementById('inputZip').value = '';
+    document.getElementById('numero de residencia').value = '';
+    document.getElementById('complemento').value = '';
+    document.getElementById('localidade').value = '';
+    document.getElementById('bairro').value = '';
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+// length é uma propriedade que verifica a quantidade de caracteres dentro do argumento cep.
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
